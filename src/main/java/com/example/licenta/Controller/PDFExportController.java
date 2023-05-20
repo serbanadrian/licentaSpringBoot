38b0d1da36ae8d4567f85565685dd752c5e49ac9
serbanadrian <adrianserban928@yahoo.com>
2023-05-20 08:13:59 +0300

    adding spring boot to app

---
+++ src/main/java/com/example/licenta/Controller/PDFExportController.java
@@ -0,0 +1,36 @@
+package com.example.licenta.Controller;
+
+import com.example.Licenta.Service.PDFGeneratorService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+public class PDFExportController {
+    private final PDFGeneratorService pdfGeneratorService;
+
+    public PDFExportController(PDFGeneratorService pdfGeneratorService) {
+        this.pdfGeneratorService = pdfGeneratorService;
+    }
+
+    @GetMapping("/pdf/generate")
+    public void generatePDF(HttpServletResponse response, @RequestParam(name = "cifra") String cifra, @RequestParam(name = "figura") String figura, @RequestParam(name = "culoare") String culoare) throws IOException, URISyntaxException {
+        response.setContentType("application/pdf");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd:hh:mm:ss");
+        String currentDateTime = dateFormat.format(new Date());
+        String headerKey = "Content_Disponsition";
+        String headerValue = "attachement; filename=pdf_" + currentDateTime + ".pdf";
+        response.setHeader(headerKey, headerValue);
+        pdfGeneratorService.setNr(Integer.valueOf(cifra));
+        pdfGeneratorService.setFigura(figura);
+        pdfGeneratorService.setCuloare(culoare);
+        this.pdfGeneratorService.crearePDF(response);
+    }
+}
---
+++ src/main/java/com/example/licenta/Service/PDFGeneratorService.java
@@ -0,0 +1,107 @@
+package com.example.licenta.Service;
+
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.PdfWriter;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+
+
+@Service
+public class PDFGeneratorService {
+    private int nr;
+    private String figura;
+    private String culoare;
+
+    public void setNr(int nr) {
+        this.nr = nr;
+    }
+
+    public void setCuloare(String culoare) {
+        this.culoare = culoare;
+    }
+
+    public void setFigura(String figura) {
+        this.figura = figura;
+    }
+
+    public void crearePDF(HttpServletResponse response) throws IOException {
+        Document document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document, response.getOutputStream());
+        document.open();
+        Font titlu = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+        titlu.setSize(23);
+        Paragraph paragraph = new Paragraph("Invatarea " + this.corectie() + nr, titlu);
+        paragraph.setAlignment(Paragraph.ALIGN_CENTER);
+        Font font = FontFactory.getFont(FontFactory.HELVETICA);
+        Paragraph paragraph1 = new Paragraph("Exersarea scrierii " + this.corectie() + nr + this.numar(), font);
+        Paragraph paragraph2 = new Paragraph("Exercitii de recunoastere a " + this.corectie() + nr + this.numar() + ":", font);
+        Paragraph paragraph3 = new Paragraph("Creati prin incercuire grupe de cate " + this.figuri(), font);
+        Paragraph paragraph4 = new Paragraph("Deseneaza ce inseamna pentru tine " + nr + this.numar());
+        Paragraph newline = new Paragraph("\n");
+
+        String writingExerciseImagePath = "static/ScrierePDF " + nr + ".png";
+        String circlingExerciseImagePath = "static/Incercuire cifra " + nr + ".png";
+        String groupExerciseImagePath = "static/" + figura + " " + culoare + ".png";
+        ClassLoader classLoader = getClass().getClassLoader();
+        Image writingExerciseImage = Image.getInstance(classLoader.getResource(writingExerciseImagePath));
+        writingExerciseImage.scaleAbsolute(450, 50);
+        Image circlingExerciseImage = Image.getInstance(classLoader.getResource(circlingExerciseImagePath));
+        circlingExerciseImage.scaleAbsolute(450, 100);
+        Image groupExerciseImage = Image.getInstance(classLoader.getResource(groupExerciseImagePath));
+        groupExerciseImage.scaleAbsolute(450, 100);
+
+        document.add(paragraph);
+        document.add(newline);
+        document.add(paragraph1);
+        document.add(newline);
+        document.add(writingExerciseImage);
+        document.add(writingExerciseImage);
+        document.add(newline);
+        document.add(paragraph2);
+        document.add(newline);
+        document.add(circlingExerciseImage);
+        document.add(newline);
+        document.add(paragraph3);
+        document.add(newline);
+        document.add(groupExerciseImage);
+        document.add(newline);
+        document.add(paragraph4);
+        document.close();
+    }
+
+    public String numar() {
+        if (nr == 1) return "(unu)";
+        if (nr == 2) return "(doi)";
+        if (nr == 3) return "(trei)";
+        if (nr == 4) return "(patru)";
+        if (nr == 5) return "(cinci)";
+        if (nr == 6) return "(sase)";
+        if (nr == 7) return "(sapte)";
+        if (nr == 8) return "(opt)";
+        if (nr == 9) return "(noua)";
+        if (nr == 10) return "(zece)";
+        return "invalid";
+    }
+
+    public String figuri() {
+        if (nr == 1) return "1(individuale):";
+        if (nr == 2) return "2(doua):";
+        if (nr == 3) return "3(trei):";
+        if (nr == 4) return "4(patru):";
+        if (nr == 5) return "5(cinci):";
+        if (nr == 6) return "6(sase):";
+        if (nr == 7) return "7(sapte):";
+        if (nr == 8) return "8(opt):";
+        if (nr == 9) return "9(noua):";
+        if (nr == 10) return "10(zece):";
+        return "invalid";
+    }
+
+    public String corectie() {
+        if (nr == 10) return "numarului ";
+        else return "cifrei ";
+    }
+}
